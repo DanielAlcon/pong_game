18792db17307cfb98c73b2d4d42e574511cfa43d
DanielAlcon <daniel.alcon.diaz@gmail.com>
2016-06-28 10:42:35 +0200

    canvas drawn. just before animating

--- pong.js
+++ pong.js
@@ -1,4 +1,4 @@
-var animate = window.RequestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function (callback) {
+var animate = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function (callback) {
 	window.setTimeout(callback, 1000/60)
 }
 
@@ -25,7 +25,64 @@ var update = function (){
 
 }
 
-var render = function () {
+/*var render = function () {
 	context.fillStyle = '#a6e5ff';
 	context.fillRect(0,0,width,height)
+}*/
+
+function Paddle (x, y, width, height){
+	this.x = x;
+	this.y = y;
+	this.width = width;
+	this.height = height;
+	this.x_speed = 0;
+	this.y_speed = 0;
+}
+
+Paddle.prototype.render = function () {
+	context.fillStyle = '#ed143d';
+	context.fillRect(this.x, this.y, this.width, this.height);
+}
+
+function Player () {
+	this.paddle = new Paddle(175, 580, 50, 10);
+}
+
+function Computer () {
+	this.paddle = new Paddle(175, 10, 50, 10);
+}
+
+Player.prototype.render = function(){
+	this.paddle.render();
+}
+
+Computer.prototype.render = function(){
+	this.paddle.render();
+}
+
+function Ball(x,y){
+	this.x = x;
+	this.y = y;
+	this.x_speed = 0;
+	this.y_speed = 3;
+	this.radius = 5;
+}
+
+Ball.prototype.render = function(){
+	context.beginPath();
+	context.arc(this.x,this.y,this.radius, 2*Math.PI, false);
+	context.fillStyle = '#000';
+	context.fill();
+}
+
+var player = new Player();
+var computer = new Computer();
+var ball = new Ball(200,300);
+
+var render = function (){
+	context.fillStyle = '#a6e5ff';
+	context.fillRect(0,0,width,height);
+	player.render();
+	computer.render();
+	ball.render();
 }
